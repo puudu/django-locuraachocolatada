2586faaaa9f7f587285a7e6b404f08442989b906
puudu <104123662+puudu@users.noreply.github.com>
2023-11-14 07:26:25 -0300

    (front) header, footer, home header

--- locuraachocolatada/tailwind.config.js
+++ locuraachocolatada/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./client/templates/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./client/templates/**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                "custom-brown": "#3B200B",
+                "custom-pink": "#F98497",
+                "custom-dark-pink": "#B3556A",
+            },
+        },
+    },
+    plugins: [],
 };
